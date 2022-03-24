ad8540f8cee89a976a0b08340a5633d27c1a00d2
Afx-Abu <100409086+Afx-Abu@users.noreply.github.com>
2022-03-24 12:15:08 +0530

    ✨ 𝙰𝙱𝚄 𝚂𝙴𝚁 𝚅2 𝚄𝙿𝙳𝙰𝚃𝙴 💯

--- plugins/_abu.js
+++ plugins/_abu.js
@@ -1,8 +1,7 @@
-
 /* Copyright (C) Yusuf Usta.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-Remoded by - Amal
+Remoded by - Abu ser
 */
 
 const Asena = require('../events');
