2bad3e96b445f7e2cb8e523b273a883d8b29c433
Afx-Abu <100409086+Afx-Abu@users.noreply.github.com>
2022-03-25 17:16:56 +0530

    ✨ 𝙰𝙱𝚄 𝚂𝙴𝚁 𝚅2 𝚄𝙿𝙳𝙰𝚃𝙴 💯

--- qr.js
+++ qr.js
@@ -8,7 +8,7 @@ WhatsAsena - Yusuf Usta
 
 const chalk = require('chalk');
 const {WAConnection, MessageOptions, MessageType} = require('@adiwajshing/baileys');
-const {StringSession} = require('./AMRU/');
+const {StringSession} = require('./Abu/');
 const fs = require('fs');
 
 async function whatsAsena () {
