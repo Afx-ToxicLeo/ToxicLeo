7d654974e66cd1b93f065a1bdf853a3b2a9e0e0b
Afx-Abu <100409086+Afx-Abu@users.noreply.github.com>
2022-03-26 20:16:56 +0530

    ✨ 𝙰𝙱𝚄 𝚂𝙴𝚁 𝚅2 𝚄𝙿𝙳𝙰𝚃𝙴 💯

--- plugins/song.js
+++ plugins/song.js
@@ -1,25 +1,31 @@
 const Asena = require('../events');
+
 const { MessageType, Mimetype } = require('@adiwajshing/baileys');
+
 const got = require('got');
-const LOAD_ING = "```Result found...```"
+
 const Config = require('../config');
+
+const LOAD_ING = "*RESULT FOUND UPLOADING...* \n\n *🛑Wait.....*"
+
 const axios = require('axios')
+
 const Axios = require('axios')
 
+Asena.addCommand({pattern: 'play ?(.*)', fromMe: false, desc: 'play song' , dontAddCommandList: true }, async (message, match) => {
 
-Asena.addCommand({pattern: 'song ?(.*)', fromMe: false, desc: 'play song' , dontAddCommandList: true }, async (message, match) => {
-	
-	await message.client.sendMessage(message.jid, '```ʜᴇʏ ʙʀᴏ ᴡᴀɪᴛ ᴄʜᴀʏʏᴜ..💫```' , MessageType.text, { quoted: message.data });
-	
+	await message.client.sendMessage(message.jid, '*SEARCHING YOU DATA*' , MessageType.text, { quoted: message.data });
+
 	const {data} = await axios(`https://zenzapi.xyz/api/play/playmp3?query=${match[1]}&apikey=whitedevil-terrorboy`)
-	
+
         const { status, result } = data
-	
+
 	var img = await Axios.get(`${result.thumb}`, {responseType: 'arraybuffer'})
-	
-        if(!status) return await message.sendMessage('```NO RESULT FOUND```')
-	
+
+        if(!status) return await message.sendMessage('*NO RESULT FOUND*')
+
         await message.client.sendMessage(message.jid, LOAD_ING , MessageType.text, { quoted: message.data });
+
         let msg = '```'
 
         msg +=  `TITLE :${result.title}\n\n`
