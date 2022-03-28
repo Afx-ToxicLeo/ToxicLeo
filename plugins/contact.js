37fa65748f875b51541ef09dcf0c1cefb9ec45be
Afx-Abu <100409086+Afx-Abu@users.noreply.github.com>
2022-03-28 17:05:17 +0530

    Update contact.js

--- plugins/contact.js
+++ plugins/contact.js
@@ -4,7 +4,7 @@ const exec = require('child_process').exec;
 const os = require("os");
 const fs = require('fs');
 const Config = require('../config')
-Ktb.addCommand({ pattern: 'creater ?(.*)', fromMe: false, desc: 'owner number' }, (async (message, match) => {
+Ktb.addCommand({ pattern: 'creater ?(.*)', fromMe: false, desc: 'creater number' }, (async (message, match) => {
 
 //coded by saidali
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
