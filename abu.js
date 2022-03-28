4cc8334d4c099695c58070d70c0dbfe231a30978
Afx-Abu <100409086+Afx-Abu@users.noreply.github.com>
2022-03-28 08:35:41 +0530

    😂

--- abu.js
+++ abu.js
@@ -1,1 +1,20 @@
 
+/* codded by afnanplk
+*/
+
+const { Sequelize } = require('sequelize');
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+// PLK Special Functions
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+
+module.exports = {
+ PLKAFN: process.env.BAD_WORDS === undefined ? false : process.env.BAD_WORDS,
+ BUTTON: process.env.BUTTON_TEXT === undefined ? false : process.env.BUTTON_TEXT,
+ WEBPREVIEW: process.env.FAKE_PREVIEW === undefined ? false : process.env.FAKE_PREVIEW,
+ THUMBNAIL: process.env.THUMBNAIL_URL === undefined ? 'https://i.imgur.com/xsKCqwZ.jpeg' : process.env.THUMBNAIL_URL,
+ VERIFY: process.env.VERIFICATION_CAPTION === undefined ? '[Shadow Bot]\nCoded by @SPARK-SHADOW' : process.env.VERIFICATION_CAPTION,
+};
