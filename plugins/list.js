3571c2292a3d7719841e44e6c1d241788ac314cb
Afx-Abu <100409086+Afx-Abu@users.noreply.github.com>
2022-03-28 16:03:26 +0530

    🤫🤣

---
+++ plugins/list.js
@@ -0,0 +1,147 @@
+/* Copyright (C) 2020 Yusuf Usta.
+re-editted by Abu
+Abu-Bot
+*/
+
+const Abu = require('../events');
+const Config = require('../config');
+const {MessageType} = require('@adiwajshing/baileys');
+const fs = require("fs")
+const axios = require('axios');
+let wk = Config.WORKTYPE == 'public' ? false : true
+const Language = require('../language');
+const Lang = Language.getString('_asena');
+const hrs = new Date().getHours({ timeZone: 'Asia/Kolkata' })
+
+if (Config.WORKTYPE == 'private') {
+
+Abu.addCommand({pattern: 'list ?(.*)', fromMe: wk, dontAddCommandList: true}, (async (message, match) => {
+var url = await axios.get(Abu.THUMBNAIL, { responseType: 'arraybuffer' })
+    var CMD_HELP = '';
+    if (match[1] === '') {
+        Abu.commands.map(
+            async (command) =>  {
+                if (command.dontAddCommandList || command.pattern === undefined) return;
+                try {
+                    var match = command.pattern.toString().match(/(\W*)([A-Za-zğüşiöç1234567890]*)/);
+                } catch {
+                    var match = [command.pattern];
+                }
+    
+                var HANDLER = '';
+    
+                if (/\[(\W*)\]/.test(Config.HANDLERS)) {
+                    HANDLER = Config.HANDLERS.match(/\[(\W*)\]/)[1][0];
+                } else {
+                    HANDLER = '.';
+                }
+                CMD_HELP += '*📓 ' + Lang.COMMAND + ':* ```' + (match.length >= 3 ? (HANDLER + match[2]) : command.pattern) + (command.desc === '' ? '```\n\n' : '```\n');
+                if (command.desc !== '') CMD_HELP += '*♦️ ' + Lang.DESC + ':* ```' + command.desc + (command.warn === '' ? '```\n\n' : '```\n');
+                if (command.usage !== '') CMD_HELP += '*📝 ' + Lang.EXAMPLE + ':* ```' + command.usage + '```\n\n';
+                if (command.warn !== '') CMD_HELP += '*⚠️ ' + Lang.WARN + ':* ```' + command.warn + '```\n\n';
+
+            }
+        );
+        
+        await message.client.sendMessage(
+            message.jid, CMD_HELP, MessageType.text , {quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(message.jid ? { remoteJid: "status@broadcast" } : {}) }, message: { "imageMessage": { "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption": Abu.VERIFY, "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1079, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": Buffer.from(url.data)}}}}
+        );    
+    } else {
+        var CMD_HELP = '';
+        Abu.commands.map(
+            async (command) =>  {
+                if (command.dontAddCommandList || command.pattern === undefined) return;
+                try {
+                    var cmatch = command.pattern.toString().match(/(\W*)([A-Za-zğüşiöç1234567890]*)/);
+                } catch {
+                    var cmatch = [command.pattern];
+                }
+                
+                if (cmatch[2] == match[1]) {
+                    var HANDLER = '';
+    
+                    if (/\[(\W*)\]/.test(Config.HANDLERS)) {
+                        HANDLER = Config.HANDLERS.match(/\[(\W*)\]/)[1][0];
+                    } else {
+                        HANDLER = '.';
+                    }
+                    CMD_HELP += '*📓 ' + Lang.COMMAND + ':* ```' + (cmatch.length >= 3 ? (HANDLER + cmatch[2]) : command.pattern) + (command.desc === '' ? '```\n\n' : '```\n');
+                    if (command.desc !== '') CMD_HELP += '*♦️ ' + Lang.DESC + ':* ```' + command.desc + (command.warn === '' ? '```\n\n' : '```\n');
+                    if (command.usage !== '') CMD_HELP += '*💎 ' + Lang.EXAMPLE + ':* ```' + command.usage + '```\n\n';
+                    if (command.warn !== '') CMD_HELP += '*⚠️ ' + Lang.WARN + ':* ```' + command.warn + '```\n\n';
+
+                }
+            }
+        );
+        if (CMD_HELP === '') CMD_HELP += Lang.NOT_FOUND;
+        await message.client.sendMessage(
+            message.jid, CMD_HELP, MessageType.text,{quoted: message.data}
+        );
+    }
+}));
+}
+
+if (Config.WORKTYPE == 'public') {
+
+Abu.addCommand({pattern: 'list ?(.*)', fromMe: wk, dontAddCommandList: true}, (async (message, match) => {
+var url = await axios.get(Abu.THUMBNAIL, { responseType: 'arraybuffer' })
+    var CMD_HELP = '';
+    if (match[1] === '') {
+        Abu.commands.map(
+            async (command) =>  {
+                if (command.dontAddCommandList || command.pattern === undefined) return;
+                try {
+                    var match = command.pattern.toString().match(/(\W*)([A-Za-zğüşiöç1234567890]*)/);
+                } catch {
+                    var match = [command.pattern];
+                }
+    
+                var HANDLER = '';
+    
+                if (/\[(\W*)\]/.test(Config.HANDLERS)) {
+                    HANDLER = Config.HANDLERS.match(/\[(\W*)\]/)[1][0];
+                } else {
+                    HANDLER = '.';
+                }
+                CMD_HELP += '*📓 ' + Lang.COMMAND + ':* ```' + (match.length >= 3 ? (HANDLER + match[2]) : command.pattern) + (command.desc === '' ? '```\n\n' : '```\n');
+                if (command.desc !== '') CMD_HELP += '*♦️ ' + Lang.DESC + ':* ```' + command.desc + (command.warn === '' ? '```\n\n' : '```\n');
+                if (command.usage !== '') CMD_HELP += '*📝 ' + Lang.EXAMPLE + ':* ```' + command.usage + '```\n\n';
+                if (command.warn !== '') CMD_HELP += '*⚠️ ' + Lang.WARN + ':* ```' + command.warn + '```\n\n';
+
+            }
+        );
+        
+        await message.client.sendMessage(
+            message.jid, CMD_HELP, MessageType.text , {quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(message.jid ? { remoteJid: "status@broadcast" } : {}) }, message: { "imageMessage": { "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption": Abu.VERIFY, "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1079, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": Buffer.from(url.data)}}}}
+        );    
+    } else {
+        var CMD_HELP = '';
+        Abu.commands.map(
+            async (command) =>  {
+                if (command.dontAddCommandList || command.pattern === undefined) return;
+                try {
+                    var cmatch = command.pattern.toString().match(/(\W*)([A-Za-zğüşiöç1234567890]*)/);
+                } catch {
+                    var cmatch = [command.pattern];
+                }
+                
+                if (cmatch[2] == match[1]) {
+                    var HANDLER = '';
+    
+                    if (/\[(\W*)\]/.test(Config.HANDLERS)) {
+                        HANDLER = Config.HANDLERS.match(/\[(\W*)\]/)[1][0];
+                    } else {
+                        HANDLER = '.';
+                    }
+                    CMD_HELP += '*📓 ' + Lang.COMMAND + ':* ```' + (cmatch.length >= 3 ? (HANDLER + cmatch[2]) : command.pattern) + (command.desc === '' ? '```\n\n' : '```\n');
+                    if (command.desc !== '') CMD_HELP += '*♦️ ' + Lang.DESC + ':* ```' + command.desc + (command.warn === '' ? '```\n\n' : '```\n');
+                    if (command.usage !== '') CMD_HELP += '*💎 ' + Lang.EXAMPLE + ':* ```' + command.usage + '```\n\n';
+                    if (command.warn !== '') CMD_HELP += '*⚠️ ' + Lang.WARN + ':* ```' + command.warn + '```\n\n';
+
+                }
+            }
+        );
+        if (CMD_HELP === '') CMD_HELP += Lang.NOT_FOUND;
+        await message.client.sendMessage(
+            message.jid, CMD_HELP, MessageType.text,{quoted: message.data}
+        );
