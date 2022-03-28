0ff50bf542dfde9f27ccb36b071625321168057f
Afx-Abu <100409086+Afx-Abu@users.noreply.github.com>
2022-03-28 09:19:13 +0530

    Creater fix

--- plugins/contact.js
+++ plugins/contact.js
@@ -4,13 +4,13 @@ const exec = require('child_process').exec;
 const os = require("os");
 const fs = require('fs');
 const Config = require('../config')
-Ktb.addCommand({ pattern: 'git ?(.*)', fromMe: false, desc: 'owner number' }, (async (message, match) => {
+Ktb.addCommand({ pattern: 'creater ?(.*)', fromMe: false, desc: 'owner number' }, (async (message, match) => {
 
 //coded by saidali
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
             + 'FN:ABU-SER [OWNER]\n' // full name
-            + 'ORG:AMRU-SER;\n' // 
+            + 'ORG:ABU-SER;\n' // 
             + 'TEL;type=CELL;type=VOICE;waid=917025994178:+91 9074994655\n' // WhatsApp ID + phone number
             + 'END:VCARD'
 await message.client.sendMessage(message.jid,{displayname: "ABU-SER [OWNER]", vcard: vcard}, MessageType.contact)
