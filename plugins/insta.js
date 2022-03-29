0d3deaf7a41e60ceffe3db4ba6499eabaf7eef04
Afx-Abu <100409086+Afx-Abu@users.noreply.github.com>
2022-03-29 21:01:26 +0530

    Update insta.js

--- plugins/insta.js
+++ plugins/insta.js
@@ -1,7 +1,7 @@
 const { MessageType, Mimetype } = require('@adiwajshing/baileys');
 const MyPnky = require('../events');
 const Config = require('../config');
-const { igDownloader, igstalk } = require('../scraper.js')
+const { igDownloader, igstalk } = require('../Abu.js')
 const { errorMessage, infoMessage } = require('../helpers');
 const axios = require('axios');
 const fs = require('fs');
