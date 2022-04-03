e4e069175481fba7e77c6ca0d25909e9a030892e
Afx-Abu <100409086+Afx-Abu@users.noreply.github.com>
2022-04-03 17:35:43 +0530

    ✨ 𝙰𝙱𝚄 𝚂𝙴𝚁 𝚅2 𝚄𝙿𝙳𝙰𝚃𝙴 💯

--- plugins/truecaller.js
+++ plugins/truecaller.js
@@ -1,86 +1,22 @@
-const Asena = require('../events');
+const axios = require ('axios');
+const {addCommand} = require('../events');
 const Config = require('../config');
-const {MessageType} = require('@adiwajshing/baileys');
-const got = require('got');
-
-// List
-const IPSTATUS_DESC = "It Sends Your IP details"
-const NEED_IP = "*Enter Your IP Address..!*"
-const IP = "IP :"
-const ST = "STATUS :"
-const CONTINENT = "CONTINENT :"
-const COUNTRY = "COUNTRY :"
-const COUNTRYCODE = "COUNTRYCODE :"
-const REGIONNAME = "REGIONNAME :"
-const CITY = "CITY :"
-const ZIP = "ZIP :"
-const CURRENCY = "CURRENCY :"
-const ISP = "ISP :"
-const MOBILE = "MOBILE :"
-const PROXY = "PROXY :"
-const NOT_FOUNDIP = "```Sorry,I could not your IP 😖```"
-
-if (Config.WORKTYPE == 'private') {
-
-  Asena.addCommand({pattern: 'true ?(.*)', desc: 'gives you the detail of your IP' ,fromMe: true}, async (message, match) => {
-
-    if (message.jid === '905524317852-1612300121@g.us') {
-
-                return;
-            }
-
-    if (match[1] === '') return await message.reply(NEED_IP);
-	const url = `https://api.techniknews.net/ipgeo/${match[1]}`;
-	try {
-		const response = await got(url);
-		const ipjson = JSON.parse(response.body);
-		if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '*🔴 ' + IP +'* ```' + match[1] + '```\n\n' +
-		'*✅' + ST +'* ```' + ipjson.status+ '```\n' +
-        '*🌐' + CONTINENT +'* ```' + ipjson.continent+ '```\n' +
-        '*🗺' + COUNTRY +'* ```' + ipjson.country+ '```\n' +
-        '*🔢' + COUNTRYCODE +'* ```' + ipjson.countryCode+ '```\n' +
-        '*🌍' + REGIONNAME +'* ```' + ipjson.regionName+ '```\n' +
-        '*🚩' + CITY +'* ```' + ipjson.city+ '```\n' +
-        '*🏛' + ZIP +'* ```' + ipjson.zip+ '```\n' +
-        '*💸' + CURRENCY +'* ```' + ipjson.currency+ '```\n\n' +
-        '*📡' + ISP +'* ```' + ipjson.isp+ '```\n' +
-        '*🛡' + PROXY +'* ```' + ipjson.proxy+ '```\n' +
-        '*📱' + MOBILE +'* ```' + ipjson.mobile+ '```\n', MessageType.text);
-	} 
-    catch {
-		return await message.client.sendMessage(message.jid, NOT_FOUNDIP, MessageType.text);
-	}
- });
-}	
-else if (Config.WORKTYPE == 'public') {
-
-  Asena.addCommand({pattern: 'true ?(.*)', desc: 'gives you the detail of your IP' ,fromMe: false}, async (message, match) => {
-
-    if (message.jid === '905524317852-1612300121@g.us') {
-
-                return;
-            }
-
-    if (match[1] === '') return await message.reply(NEED_IP);
-	const url = `https://api.techniknews.net/ipgeo/${match[1]}`;
-	try {
-		const response = await got(url);
-		const ipjson = JSON.parse(response.body);
-		if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '*🔴 ' + IP +'* ```' + match[1] + '```\n\n' +
-		'*✅' + ST +'* ```' + ipjson.status+ '```\n' +
-        '*🌐' + CONTINENT +'* ```' + ipjson.continent+ '```\n' +
-        '*🗺' + COUNTRY +'* ```' + ipjson.country+ '```\n' +
-        '*🔢' + COUNTRYCODE +'* ```' + ipjson.countryCode+ '```\n' +
-        '*🌍' + REGIONNAME +'* ```' + ipjson.regionName+ '```\n' +
-        '*🚩' + CITY +'* ```' + ipjson.city+ '```\n' +
-        '*🏛' + ZIP +'* ```' + ipjson.zip+ '```\n' +
-        '*💸' + CURRENCY +'* ```' + ipjson.currency+ '```\n\n' +
-        '*📡' + ISP +'* ```' + ipjson.isp+ '```\n' +
-        '*🛡' + PROXY +'* ```' + ipjson.proxy+ '```\n' +
-        '*📱' + MOBILE +'* ```' + ipjson.mobile+ '```\n', MessageType.text);
-	} 
-    catch {
-		return await message.client.sendMessage(message.jid, NOT_FOUNDIP, MessageType.text);
-	}
- });
-}
+const Abu = Config.WORKTYPE == 'public' ? false : true
+addCommand({ pattern: 'true ?(.*)', fromMe: true, desc: "get number details" }, async (msg, query) => {
+if (!query[1] && !msg.reply_message) return await msg.reply("_Give me any number or reply to any user!_");  
+if (query[1].includes('/')) return await msg.client.sendMessage(msg.jid, 'Wrong format! \n\n .true +91 6282344739', MessageType.text, {quoted: msg.data})
+var go;
+if (msg.reply_message) go = msg.reply_message.jid.split('@')[0]
+else if (!query[1].includes('@')) go = query[1]
+else if (msg.mention) {
+var mm = '';
+msg.mention.map(async (user) => {
+mm += user.split('@')[0];
+});
+go = mm
+} 
+var initt = go.split(" ").join("")
+var number = initt.replace('+','')
+var {data} = await axios(`https://raganork-api.herokuapp.com/api/truecaller?text=in&username=${number}&apikey=souravkl11`);
+await msg.sendMessage(data.result.data[0] == 'error' ? 'Server limit over (50/50)' : data.result.data[0])
+})
