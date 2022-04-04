e50643893e1ee316dff1371feb00ee0ed7635b1a
Afx-Abu <100409086+Afx-Abu@users.noreply.github.com>
2022-04-04 07:38:21 +0530

    Create test.js

---
+++ plugins/test.js
@@ -0,0 +1,13 @@
+// # BUFFER - TEST
+
+const Abu = require('../events');
+const { MessageType, Mimetype } = require('@adiwajshing/baileys');
+const fs = require('fs');
+const axios = require('axios');
+let wk = Config.WORKTYPE == 'public' ? false : true
+
+Abu.addCommand({pattern: 'test ?(.*)', fromMe: wk, dontAddCommandList: true }, (async (message, match) => {
+const AbuBuffer = await axios.get(`https://i.imgur.com/nLUMShe.jpg`, { responseType: 'arraybuffer' })
+await message.client.sendMessage(message.jid, Buffer.from(AbuBuffer.data), MessageType.image, {mimetype: Mimetype.jpg })
+
+}));
