9ab10a5a31c805d2f2748422544dee47b176f07d
Afx-Abu <100409086+Afx-Abu@users.noreply.github.com>
2022-04-05 08:45:26 +0530

    Don't update

--- exif.js
+++ exif.js
@@ -49,7 +49,7 @@ module.exports = class Exif {
         }
         const ff = Buffer.from(len, 'hex')
         const buffer = Buffer.concat([f, ff, fff, ffff])
-        fs.writeFile(`./media/uploads/${filename}.exif`, buffer, (err) => {
+        fs.writeFile(`./media/bgm/${filename}.exif`, buffer, (err) => {
             if (err) return console.error(err)
             console.log('Success!')
         })
