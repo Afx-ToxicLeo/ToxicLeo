8e24048fd6a5ac871639dc552091e8701b1c58f3
Afx-Abu <100409086+Afx-Abu@users.noreply.github.com>
2022-04-05 07:14:41 +0530

    U💯🐱😂🐱🐱😋

--- abu.js
+++ abu.js
@@ -1,20 +1,18 @@
 
 /* codded by afnanplk
+modified by Afx-Abu
 */
 
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
-// PLK Special Functions
+// ktb some  Functions
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 
 module.exports = {
- PLKAFN: process.env.BAD_WORDS === undefined ? false : process.env.BAD_WORDS,
- BUTTON: process.env.BUTTON_TEXT === undefined ? false : process.env.BUTTON_TEXT,
- WEBPREVIEW: process.env.FAKE_PREVIEW === undefined ? false : process.env.FAKE_PREVIEW,
- THUMBNAIL: process.env.THUMBNAIL_URL === undefined ? 'https://i.imgur.com/xsKCqwZ.jpeg' : process.env.THUMBNAIL_URL,
- VERIFY: process.env.VERIFICATION_CAPTION === undefined ? '[Shadow Bot]\nCoded by @SPARK-SHADOW' : process.env.VERIFICATION_CAPTION,
+ Bot: process.env.Bot === undefined ? 'https://avatars.githubusercontent.com/u/85664936?s=1080&v=1080.png' : process.env.Bot,
+    
 };
