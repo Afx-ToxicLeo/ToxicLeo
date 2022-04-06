1bdec5a3c2be39a1705e3b31d6e9168b4572945e
Afx-Abu <100409086+Afx-Abu@users.noreply.github.com>
2022-04-06 20:08:49 +0530

    Update abu.js

--- abu.js
+++ abu.js
@@ -14,5 +14,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
  Bot: process.env.Bot === undefined ? 'https://avatars.githubusercontent.com/u/85664936?s=1080&v=1080.png' : process.env.Bot,
+ THUMBNAIL: process.env.THUMBNAIL_URL === undefined ? 'https://i.imgur.com/xsKCqwZ.jpeg' : process.env.THUMBNAIL_URL,
+ VERIFY: process.env.VERIFICATION_CAPTION === undefined ? '[ABU SER]\nCoded by @AFX-ABU' : process.env.VERIFICATION_CAPTION,
     
 };
