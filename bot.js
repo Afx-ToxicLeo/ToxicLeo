673ed4620147ed57e9e744c48baa6a181ec73c14
Afx-Abu <100409086+Afx-Abu@users.noreply.github.com>
2022-04-13 12:32:39 +0530

    𝙰𝙱𝚄 𝚂𝙴𝚁 𝚅2 𝚄𝙿𝙳𝙰𝚃𝙴 𝙰𝚅𝙴𝙻𝙸𝙱𝙻𝙴🌼

--- bot.js
+++ bot.js
@@ -212,8 +212,17 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp...')}`);
         let msg = m.messages.all()[0];
         if (msg.key && msg.key.remoteJid == 'status@broadcast') return;
 
-        if (config.NO_ONLINE) {
+        if (config.BOT_PRESENCE == 'offline') {
             await conn.updatePresence(msg.key.remoteJid, Presence.unavailable);
+        
+        } else if (config.BOT_PRESENCE == 'online') {
+            await conn.updatePresence(msg.key.remoteJid, Presence.available);
+        
+        } else if (config.BOT_PRESENCE == 'typing') {
+            await conn.updatePresence(msg.key.remoteJid, Presence.composing);
+        
+        } else if (config.BOT_PRESENCE == 'recording') {
+            await conn.updatePresence(msg.key.remoteJid, Presence.recording);
         }
 
       // Thanks to farhan dqz, souravl11, afnanplk
