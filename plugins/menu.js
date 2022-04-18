2c72e86b50c67777110ec6d649b1159d474c37a2
Afx-Abu <100409086+Afx-Abu@users.noreply.github.com>
2022-04-18 07:18:09 +0530

    💫Fix

--- plugins/menu.js
+++ plugins/menu.js
@@ -17,24 +17,6 @@ const axios = require('axios');
 const Language = require('../language');
 const Lang = Language.getString('system_stats');
 
-
-if (Config.WORKTYPE == 'private') {
-
-    Abu.addCommand({pattern: 'alive', fromMe: true, desc: Lang.ALIVE_DESC}, (async (message, match) => {
-        
-        let pp
-        try { pp = await message.client.getProfilePicture(message.jid.includes('-') ? message.data.participant : message.jid ); } catch { pp = await message.client.getProfilePicture(); }
-        await axios.get(pp, {responseType: 'arraybuffer'}).then(async (res) => { await message.client.sendMessage(message.jid, res.data, MessageType.image, { caption: Config.MENUMSG }); });
-    }));
-
-    Abu.addCommand({pattern: 'sysd', fromMe: true, desc: Lang.SYSD_DESC}, (async (message, match) => {
-
-        const child = spawnSync('neofetch', ['--stdout']).stdout.toString('utf-8')
-        await message.sendMessage(
-            '```' + child + '```', MessageType.text
-        );
-    }));
-}
 else if (Config.WORKTYPE == 'public') {
 
    Abu.addCommand({pattern: 'menu', fromMe: true, desc: Lang.ALIVE_DESC}, (async (message, match) => {
