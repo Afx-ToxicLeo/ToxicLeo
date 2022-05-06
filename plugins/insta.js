765d61d65b2a1c3fd56045319f90ab9a5d061410
Abu! <100409086+Afx-Abu@users.noreply.github.com>
2022-05-06 06:26:14 +0530

    Fix

--- plugins/insta.js
+++ plugins/insta.js
@@ -1,1 +1,73 @@
-const _0x2e8812=_0x3ac8;(function(_0x3527a3,_0x360d88){const _0x47125c=_0x3ac8,_0x3d80a1=_0x3527a3();while(!![]){try{const _0x11daa5=parseInt(_0x47125c(0xaf))/0x1+-parseInt(_0x47125c(0xa2))/0x2*(-parseInt(_0x47125c(0xdc))/0x3)+-parseInt(_0x47125c(0xb8))/0x4*(parseInt(_0x47125c(0xb1))/0x5)+-parseInt(_0x47125c(0xc0))/0x6+-parseInt(_0x47125c(0xb9))/0x7+parseInt(_0x47125c(0xb7))/0x8*(-parseInt(_0x47125c(0xbd))/0x9)+-parseInt(_0x47125c(0xbb))/0xa*(-parseInt(_0x47125c(0xac))/0xb);if(_0x11daa5===_0x360d88)break;else _0x3d80a1['push'](_0x3d80a1['shift']());}catch(_0x11a150){_0x3d80a1['push'](_0x3d80a1['shift']());}}}(_0x61cc,0xf14de));const _0x528fb4=(function(){let _0x592bcf=!![];return function(_0x1e0bb9,_0x2362fc){const _0x11d30=_0x592bcf?function(){const _0x5ab75c=_0x3ac8;if(_0x2362fc){const _0xd5225c=_0x2362fc[_0x5ab75c(0xd1)](_0x1e0bb9,arguments);return _0x2362fc=null,_0xd5225c;}}:function(){};return _0x592bcf=![],_0x11d30;};}()),_0x5ef46d=_0x528fb4(this,function(){const _0x361a78=_0x3ac8;return _0x5ef46d[_0x361a78(0xb0)]()[_0x361a78(0xd5)](_0x361a78(0xbf))[_0x361a78(0xb0)]()[_0x361a78(0xc4)](_0x5ef46d)[_0x361a78(0xd5)](_0x361a78(0xbf));});_0x5ef46d();const _0x319056=(function(){let _0x162bc1=!![];return function(_0x278034,_0x2cfacb){const _0x4b6849=_0x162bc1?function(){const _0x106dc4=_0x3ac8;if(_0x2cfacb){const _0x3ae456=_0x2cfacb[_0x106dc4(0xd1)](_0x278034,arguments);return _0x2cfacb=null,_0x3ae456;}}:function(){};return _0x162bc1=![],_0x4b6849;};}());(function(){_0x319056(this,function(){const _0xc67b91=_0x3ac8,_0x367166=new RegExp('function\x20*\x5c(\x20*\x5c)'),_0x569028=new RegExp(_0xc67b91(0xae),'i'),_0x288407=_0x42e004(_0xc67b91(0xd4));!_0x367166['test'](_0x288407+_0xc67b91(0xd6))||!_0x569028[_0xc67b91(0xde)](_0x288407+'input')?_0x288407('0'):_0x42e004();})();}());const _0xcfa14c=(function(){let _0xb6ae5c=!![];return function(_0x1b32c2,_0x35259b){const _0x2d3d70=_0xb6ae5c?function(){if(_0x35259b){const _0x3569f0=_0x35259b['apply'](_0x1b32c2,arguments);return _0x35259b=null,_0x3569f0;}}:function(){};return _0xb6ae5c=![],_0x2d3d70;};}()),_0x2ac6ab=_0xcfa14c(this,function(){const _0x35d67e=_0x3ac8;let _0xeadb9c;try{const _0x34f345=Function(_0x35d67e(0xba)+_0x35d67e(0xbc)+');');_0xeadb9c=_0x34f345();}catch(_0x5730e7){_0xeadb9c=window;}const _0x58ccff=_0xeadb9c[_0x35d67e(0xc3)]=_0xeadb9c[_0x35d67e(0xc3)]||{},_0x40c56d=['log','warn',_0x35d67e(0xc1),_0x35d67e(0xd7),_0x35d67e(0xab),'table',_0x35d67e(0xdf)];for(let _0x1d896a=0x0;_0x1d896a<_0x40c56d[_0x35d67e(0xda)];_0x1d896a++){const _0x5f1d19=_0xcfa14c[_0x35d67e(0xc4)][_0x35d67e(0xa1)][_0x35d67e(0xd9)](_0xcfa14c),_0x1ecb49=_0x40c56d[_0x1d896a],_0x14f0f7=_0x58ccff[_0x1ecb49]||_0x5f1d19;_0x5f1d19[_0x35d67e(0xc9)]=_0xcfa14c[_0x35d67e(0xd9)](_0xcfa14c),_0x5f1d19['toString']=_0x14f0f7['toString'][_0x35d67e(0xd9)](_0x14f0f7),_0x58ccff[_0x1ecb49]=_0x5f1d19;}});_0x2ac6ab();const got=require(_0x2e8812(0xc2)),axios=require(_0x2e8812(0xa7)),Config=require(_0x2e8812(0xcc)),anything=require(_0x2e8812(0xb6)),{addCommand}=require(_0x2e8812(0xcb)),{MessageType}=require(_0x2e8812(0xca));let sn=Config[_0x2e8812(0xa3)]==_0x2e8812(0xcd)?![]:!![];function _0x61cc(){const _0x28bec8=['../config','public','string','\x0a_*ʙɪᴏ\x20:*_\x20','call','apply','from','arraybuffer','init','search','chain','error','then','bind','length','data','1158oUzran','_*ᴜsᴇʀ\x20ɴᴀᴍᴇ\x20:*_\x20','test','trace','prototype','2534vzaHMj','WORKTYPE','ig\x20?(.*)','Insta\x20Profile\x20Search','\x0a_*©\x20ɪɴsᴛᴀ\x20ᴘʀᴏғɪʟᴇ™*_','axios','gger','get','\x0a_*ғᴏʟʟᴏᴡᴇʀs\x20:*_\x20','exception','57055647pbBYSg','client','\x5c+\x5c+\x20*(?:[a-zA-Z_$][0-9a-zA-Z_$]*)','1089525PjGWKu','toString','5GqLAjZ','catch','https://early-pie-production.up.railway.app/ig?q=','sendMessage','debu','../bgms/abu','8gIWiHG','3412804qkrxNW','13826372UMvHdW','return\x20(function()\x20','10WSSYUR','{}.constructor(\x22return\x20this\x22)(\x20)','10514979xfhDiX','while\x20(true)\x20{}','(((.+)+)+)+$','10682124dSWbDk','info','got','console','constructor','result','text','jid','\x0a_*ɴᴀᴍᴇ\x20:*_\x20','__proto__','@adiwajshing/baileys','../events'];_0x61cc=function(){return _0x28bec8;};return _0x61cc();}addCommand({'pattern':_0x2e8812(0xa4),'fromMe':sn,'desc':_0x2e8812(0xa5)},async(_0x768934,_0x34c9ad)=>{const _0x21576d=_0x2e8812,_0x33cb0f=_0x34c9ad[0x1];if(_0x33cb0f==='')return await _0x768934[_0x21576d(0xad)][_0x21576d(0xb4)](_0x768934[_0x21576d(0xc7)],'_Give\x20me\x20a\x20instagram\x20username_',MessageType[_0x21576d(0xc6)]);await axios[_0x21576d(0xa9)](_0x21576d(0xb3)+_0x33cb0f)[_0x21576d(0xd8)](async _0x404347=>{const _0x4d51a7=_0x21576d,{name:_0x5967f1,username:_0x384518,avatar:_0x1237a3,posts:_0x4be975,following:_0x1734ea,followers:_0x42a570,description:_0x4a2b5a}=_0x404347['data'][_0x4d51a7(0xc5)],_0x6a8296=await axios[_0x4d51a7(0xa9)](_0x1237a3,{'responseType':_0x4d51a7(0xd3)}),_0x2c2032=_0x4d51a7(0xdd)+_0x384518+_0x4d51a7(0xc8)+_0x5967f1+_0x4d51a7(0xcf)+_0x4a2b5a+'\x0a_*ᴘᴏsᴛs\x20:*_\x20'+_0x4be975+_0x4d51a7(0xaa)+_0x42a570+'\x0a_*ғᴏʟʟᴏᴡɴɪɴɢ\x20:*_\x20'+_0x1734ea+_0x4d51a7(0xa6);await _0x768934[_0x4d51a7(0xb4)](Buffer[_0x4d51a7(0xd2)](_0x6a8296[_0x4d51a7(0xdb)]),MessageType['image'],{'caption':_0x2c2032});})[_0x21576d(0xb2)](async _0x36adb5=>await _0x768934['client'][_0x21576d(0xb4)](_0x768934[_0x21576d(0xc7)],'_Not\x20Found_',MessageType[_0x21576d(0xc6)]));});function _0x3ac8(_0x564d83,_0x4fcd14){const _0x3cfb69=_0x61cc();return _0x3ac8=function(_0x2ac6ab,_0xcfa14c){_0x2ac6ab=_0x2ac6ab-0xa1;let _0x9474fe=_0x3cfb69[_0x2ac6ab];return _0x9474fe;},_0x3ac8(_0x564d83,_0x4fcd14);}function _0x42e004(_0x982121){function _0x3a0832(_0x3c078f){const _0x5cc1fb=_0x3ac8;if(typeof _0x3c078f===_0x5cc1fb(0xce))return function(_0x10da91){}[_0x5cc1fb(0xc4)](_0x5cc1fb(0xbe))[_0x5cc1fb(0xd1)]('counter');else(''+_0x3c078f/_0x3c078f)['length']!==0x1||_0x3c078f%0x14===0x0?function(){return!![];}['constructor'](_0x5cc1fb(0xb5)+'gger')[_0x5cc1fb(0xd0)]('action'):function(){return![];}[_0x5cc1fb(0xc4)](_0x5cc1fb(0xb5)+_0x5cc1fb(0xa8))[_0x5cc1fb(0xd1)]('stateObject');_0x3a0832(++_0x3c078f);}try{if(_0x982121)return _0x3a0832;else _0x3a0832(0x0);}catch(_0x5c7256){}}
+const skl = require('../events');
+const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
+const fs = require('fs');
+const got = require("got");
+const axios = require('axios');
+const setting = require('../config');
+const {getPost,getStalk,getStory,skbuffer} = require('raganork-bot');
+const Config = require('../config');
+const s = require('../config');
+var v = s.CHANNEL
+var need = "*_Need instagram link!_*";
+var downloading = "_Downloading_";
+var need_acc = "*_Need an instagram username!_*";
+var fail = "*_Download failed! Check your link and try again_*";
+var need_acc_s = "_Need an instagram username or link!_";
+let sourav = setting.WORKTYPE == 'public' ? false : true
+skl.addCommand({ pattern: 'insta ?(.*)', fromMe: sourav, desc:'Downloads post/reel/igtv from instagram',usage:'insta link or reply to a link'}, (async (msg, query) => {
+var q = !msg.reply_message.message ? query[1] : msg.reply_message.message
+if (!q)  return await msg.client.sendMessage(msg.jid, '_Unable to read link from message!_', MessageType.text, {quoted: msg.data});
+if (q && !q.includes('instagram.com')) return await msg.client.sendMessage(msg.jid, need, MessageType.text, {quoted: msg.data});
+var getid = /(?:https?:\/\/)?(?:www\.)?(?:instagram\.com(?:\/.+?)?\/(p|reel|tv)\/)([\w-]+)(?:\/)?(\?.*)?$/
+var url = getid.exec(q)
+if (url != null) {
+var res = await getPost(url[0],v )
+if (res === "false") return await msg.client.sendMessage(msg.jid, fail, MessageType.text, {quoted: msg.data});
+else await msg.client.sendMessage(msg.jid, downloading, MessageType.text, {quoted: msg.data});
+var url = res.data
+for (var i = 0; i < (url.length); i++) {
+var get = got(url[i], {https: {rejectUnauthorized: false}});
+var type = url[i].includes('mp4') ? MessageType.video : MessageType.image
+var mime = url[i].includes('mp4') ? Mimetype.mp4 : Mimetype.jpg
+var stream = get.buffer();
+stream.then(async (video) => {
+await msg.client.sendMessage(msg.jid, video, type, { mimetype: mime, quoted: msg.data});
+})};}
+else if (url == null) {
+var linksplit = q.split('https://')[1]
+var res = await getPost('https://'+linksplit,v )
+if (res === "false") return await msg.client.sendMessage(msg.jid, fail, MessageType.text, {quoted: msg.data});
+else await msg.client.sendMessage(msg.jid, downloading, MessageType.text, {quoted: msg.data});
+var buffer = await skbuffer(res.links[0].url)
+if (res.links[0].url.includes('mp4')) return await msg.client.sendMessage(msg.jid, buffer, MessageType.video, { mimetype: Mimetype.mp4, quoted: msg.data});
+if (res.links[0].url.includes('jpg')) return await msg.client.sendMessage(msg.jid, buffer, MessageType.image, { mimetype: Mimetype.jpg, quoted: msg.data});
+    
+}
+}));
+skl.addCommand({ pattern: 'ig ?(.*)', fromMe: sourav, desc:'Gets account info from instagram',usage:'ig username'}, (async (msg, query) => {
+    if (query[1] === '') return await msg.client.sendMessage(msg.jid, need_acc, MessageType.text, {quoted: msg.data});
+    var res = await getStalk(query[1])
+    if (res === "false") return await msg.client.sendMessage(msg.jid, "_Username invalid!_", MessageType.text, {quoted: msg.data})
+    var buffer = await skbuffer(res.hd_profile_pic_url_info.url)
+    await msg.client.sendMessage(msg.jid, buffer, MessageType.image, { mimetype: Mimetype.jpg, caption: '_*Name:*_ ' + `${res.fullname}` + '\n _*Bio:*_ ' + `${res.biography}`+ '\n _*Private account:*_ ' + `${res.is_private} ` + '\n _*Followers:*_ ' + `${res.followers}` + '\n _*Following:*_ ' + `${res.following}` + '\n _*Posts:*_ ' + `${res.post_count}` + '\n _*Verified:*_ ' + `${res.is_verified} ` + '\n _*IGTV videos:*_ ' + `${res.total_igtv_videos}`, quoted: msg.data});
+    }));
+skl.addCommand({ pattern: 'story ?(.*)', fromMe: sourav, desc:'Downloads full/single story from instagram',usage:'.story username or link'}, (async (msg, query) => {
+if (query[1] === '') return await msg.client.sendMessage(msg.jid, need_acc_s, MessageType.text, {quoted: msg.data});
+var user = query[1];
+var res = await getStory(user,v)
+if (res === "false") return await msg.client.sendMessage(msg.jid, "_Story not found!_", MessageType.text, {quoted: msg.data})
+if (res.error) return await msg.client.sendMessage(msg.jid, res.error.replace('status','story'), MessageType.text, {quoted: msg.data})
+var url = ''
+await msg.sendMessage('```Downloading '+res.result.stories.length+' stories of '+res.result.username+'```');
+res.result.stories.map((result) => {
+url += result.url + ','});
+var que = url !== false ? url.split(',') : [];
+for (var i = 0; i < (que.length < res.result.stories.length ? que.length : res.result.stories.length); i++) {
+var get = got(que[i], {https: {rejectUnauthorized: false}});
+var type = que[i].includes('mp4') ? MessageType.video : MessageType.image
+var mime = que[i].includes('mp4') ? Mimetype.mp4 : Mimetype.jpg
+var stream = get.buffer();
+stream.then(async (video) => {
+await msg.client.sendMessage(msg.jid, video, type, { mimetype: mime,quoted: msg.data});
+})};
+}));
