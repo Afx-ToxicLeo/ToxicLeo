f21b589227a4c79bfd5feff27d99618aeccc3645
('Jsl') <100409086+Afx-Abu@users.noreply.github.com>
2022-08-16 20:47:16 +0530

    Create url.js

---
+++ plugins/url.js
@@ -0,0 +1,10 @@
+const skl = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const Config = require('../config');
+
+let sk = Config.WORKTYPE == 'public' ? false : true
+skl.addCommand({pattern: 'url ?(.*)', fromMe: sk}, async (message, match) => {
+
+    await message.sendMessage('Use this site to get URL 👇:\n\n https://souravkl11.github.io/image-uploader/')
+
+        });
