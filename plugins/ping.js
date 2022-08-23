ae1589de990a0db68bdea99a39effb43f08d6ad1
('Jsl') <100409086+Afx-Abu@users.noreply.github.com>
2022-08-23 16:07:56 +0530

    Fix

---
+++ plugins/ping.js
@@ -0,0 +1,18 @@
+const { bot } = require('../events')
+
+bot(
+	{
+		pattern: 'ping ?(.*)',
+		fromMe: true,
+		desc: 'To check ping',
+		type: 'misc',
+	},
+	async (message, match) => {
+		const start = new Date().getTime()
+		await message.send('```Ping!```')
+		const end = new Date().getTime()
+		return await message.send(
+			'*Pong!*\n ```' + (end - start) + '``` *ms*'
+		)
+	}
+)
