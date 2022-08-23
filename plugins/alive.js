caa55bfd9d0330841bced7f1aca9637659fb823b
('Jsl') <100409086+Afx-Abu@users.noreply.github.com>
2022-08-23 16:07:12 +0530

    Fux

--- plugins/alive.js
+++
@@ -1,5 +0,0 @@
-var a = require('../events');
-const {MessageType, Mimetype, MessageOptions} = require('@adiwajshing/baileys');
-a.addCommand({pattern: 'alive ?(.*)', fromMe: true, desc: 'Send Alive Msg'}, async (message, match) => {
-await message.client.sendMessage(message.jid, { text: 'Im Alive' })
-});
