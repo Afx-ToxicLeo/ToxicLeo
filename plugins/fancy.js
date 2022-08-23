2f3d4e161749f0f65473eccc4a925ab145cd3c2f
('Jsl') <100409086+Afx-Abu@users.noreply.github.com>
2022-08-23 18:05:16 +0530

    Fiz

--- plugins/fancy.js
+++ plugins/fancy.js
@@ -1,7 +1,7 @@
 const {addCommand} = require('../events')
 const {Fancy} = require('abu-bot')
-const {WORKTYPE} = require('../config');
-var w = WORKTYPE == 'public'? false:true
+const {MODE} = require('../config');
+var w = MODE == 'public'? false:true
 addCommand({pattern: 'fancy ?(.*)', fromMe: w, desc: 'Creates fancy text fonts'}, (async (message, match) => {
 if (!match[1]||!message.reply_message.message) return await message.sendReply("Reply to any message with .fancy number\n"+Fancy("example","32"))
 try { var result = Fancy(message.reply_message.message,match[1]) } catch (e) {return await message.sendReply(e.message)}
