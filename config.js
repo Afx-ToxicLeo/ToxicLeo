3e2c1320851be0fe1063f9a27bee23355c2d9402
('Jsl') <100409086+Afx-Abu@users.noreply.github.com>
2022-09-12 21:13:33 +0530

    Fix deley

--- config.js
+++ config.js
@@ -1,31 +1,19 @@
-const fs = require('fs');
 const toBool = (x) => x == 'true'
-const { Sequelize } = require('sequelize');
-if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
-function convertToBool(text, fault = 'true') {
-    return text === fault ? true : false;
-}
+const { Sequelize } = require('sequelize')
+const { existsSync } = require('fs')
+if (existsSync('config.env')) require('dotenv').config({ path: './config.env' })
 const DATABASE_URL = process.env.DATABASE_URL === undefined ? './database.db' : process.env.DATABASE_URL
-DEBUG = process.env.DEBUG === undefined ? false : convertToBool(process.env.DEBUG)
 module.exports = {
-    VERSION: 'v1.0.0', 
-    SESSION_ID: process.env.SESSION_ID || '',
+    VERSION: 'v1.0.0',
+    SESSION_ID: (process.env.SESSION_ID || '').trim(),
     MODE: process.env.MODE || 'private',
-    HANDLERS: (process.env.PREFIX || '^[.,!]').trim(),
-    SEND_READ: process.env.READ_COMMAND || false, 
-    MSG_LOG: convertToBool(process.env.LOG_MSG) || false, 
-    ALWAYS_ONLINE: toBool(process.env.ALWAYS_ONLINE),
-    LANGUAGE: process.env.LANGUAGE || 'en',
-    STICKER_PACKNAME: process.env.STICKER_PACKNAME || 'Abu;Jsl',
-    ERROR_MESSAGE: convertToBool(process.env.ERROR_MESSAGE) || true, 
-    WARN: process.env.WARN || '4',
-    HEROKU: {
-        HEROKU: process.env.HEROKU === undefined ? false : convertToBool(process.env.HEROKU),
-        API_KEY: process.env.HEROKU_API_KEY || '',
-        APP_NAME: process.env.HEROKU_APP_NAME || ''
-    },
-    DATABASE_URL: DATABASE_URL,
     DATABASE: DATABASE_URL === './database.db' ? new Sequelize({ dialect: 'sqlite', storage: DATABASE_URL, logging: false }) : new Sequelize(DATABASE_URL, {dialect: 'postgres', ssl: true, protocol: 'postgres', dialectOptions: { native: true, ssl: { require: true, rejectUnauthorized: false },}, logging: false }),
+    HANDLERS: (process.env.PREFIX || '^[.,!]').trim(),
     SUDO: process.env.SUDO || '',
-    DEBUG: DEBUG
+    HEROKU_APP_NAME: process.env.HEROKU_APP_NAME,
+    HEROKU_API_KEY: process.env.HEROKU_API_KEY,
+    STICKER_PACKNAME: process.env.STICKER_PACKNAME || 'Abu;Jsl',
+    LOG_MSG: toBool(process.env.LOG_MSG) || false,
+    LANG: (process.env.LANGUAGE || 'english').toLowerCase(),
+    WARN_LIMIT: process.env.WARN_LIMIT || '3',
 };
