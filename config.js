c2716a2f4a68132d205a0ab7f628add6c2a46a55
('Jsl') <100409086+Afx-Abu@users.noreply.github.com>
2022-09-16 22:18:18 +0530

    .

--- config.js
+++ config.js
@@ -10,6 +10,8 @@ module.exports = {
     DATABASE: DATABASE_URL === './database.db' ? new Sequelize({ dialect: 'sqlite', storage: DATABASE_URL, logging: false }) : new Sequelize(DATABASE_URL, {dialect: 'postgres', ssl: true, protocol: 'postgres', dialectOptions: { native: true, ssl: { require: true, rejectUnauthorized: false },}, logging: false }),
     HANDLERS: (process.env.PREFIX || '^[.,!]').trim(),
     SUDO: process.env.SUDO || '',
+    WELCOME_MSG:'{pp}Hi @user ᴡᴇʟᴄᴏᴍᴇ @gname\nᴛᴏ\'ʀᴇ ᴏᴜʀ @count/513 ᴍᴇᴍʙᴇʀs ',
+    GOODBYE_MSG:'ʙʏ ʙʀᴏ @user ᴡʜʏ ʟᴇᴀᴠᴇᴅ',
     HEROKU_APP_NAME: process.env.HEROKU_APP_NAME,
     HEROKU_API_KEY: process.env.HEROKU_API_KEY,
     STICKER_PACKNAME: process.env.STICKER_PACKNAME || 'Abu;Jsl',
