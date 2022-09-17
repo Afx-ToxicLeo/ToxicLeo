de0f2cb28466fb5164f220f3d8d38cf57bfe5965
('Jsl') <100409086+Afx-Abu@users.noreply.github.com>
2022-09-17 07:19:33 +0530

    .fix plugins

---
+++ plugins/_menu.js
@@ -0,0 +1,32 @@
+const events = require("../lib/utils");
+const { Jsl, getBuffer } = require("../lib");
+const { readFileSync } = require("fs");
+Jsl(
+  {
+    pattern: "menu ?(.*)",
+    fromMe: true,
+    desc: "Show All commands",
+  },
+  async (message, match) => {
+    let menu = `╭╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼\n╽`
+    let cmnd = [];
+    events.commands.map((command, num) => {
+      let cmd;
+      if (command.pattern) {
+        cmd = command.pattern
+          .toString()
+          .match(/(\W*)([A-Za-züşiğ öç1234567890]*)/)[2];
+      }
+
+      if (!command.dontAddCommandList&& cmd !==undefined) {
+        cmnd.push(cmd);
+      }
+    });
+    cmnd.sort();
+    cmnd.forEach((cmd, num) => {
+      menu += `\n┠${(num += 1)} \`\`\`${cmd}\`\`\` \n╿`;
+    });
+    menu += `\n╰╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼`
+    message.sendMessage(readFileSync("./temp/abu.png"), {caption:menu},'image');
+  }
+);
