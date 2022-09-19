cef53e40051078f4994d8f6b28354358b549827c
('Jsl') <100409086+Afx-Abu@users.noreply.github.com>
2022-09-19 07:06:29 +0530

    Fix

--- plugins/fancy.js
+++ plugins/fancy.js
@@ -10,7 +10,7 @@ const {
      use: 'misc',
      desc: 'Creates fancy text fonts'
  }, (async (message, match) => {
-     if (!match[1] || !message.reply_message.message) return await message.sendReply("Reply to any message with .fancy number\n" + Fancy("example", "32"))
+     if (!match[1] || !message.reply_message.message) return await message.sendMessage("Reply to any message with .fancy number\n" + Fancy("example", "32"))
      try {
          var result = Fancy(message.reply_message.message, match[1])
      } catch (e) {
