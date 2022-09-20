fc4e96309e721de5645f10e80cc5281b97155ad2
('Jsl') <100409086+Afx-Abu@users.noreply.github.com>
2022-09-20 08:36:44 +0530

    Update list.js

--- plugins/list.js
+++ plugins/list.js
@@ -53,7 +53,7 @@ Jsl({pattern: 'assist ?(.*)', fromMe: true, dontAddCommandList: true}, (async (m
                 }
             );
             var msg_cmd = FancyRandom(CMD_HELP,Config.SESSION)
-            message.sendMessage(msg_cmd, {
+           await message.sendMessage(CMD_HELP, {
       quoted: {
         key: {
           participant: "0@s.whatsapp.net",
@@ -74,79 +74,3 @@ Jsl({pattern: 'assist ?(.*)', fromMe: true, dontAddCommandList: true}, (async (m
     });
   }
 );
-        else {
-            var CMD_HELP = '';
-            Jsl.commands.map(
-                async (command) =>  {
-                    if (command.dontAddCommandList || command.pattern === undefined) return;
-                    try {
-                        var cmatch = command.pattern.toString().match(/(\W*)([A-Za-zğüşıiöç1234567890 ]*)/);
-                        var cmmatch = command.pattern.toString().match(/(\W*)([A-Za-züşiğ öç1234567890]*)/)[2]
-                    } catch {
-                        var cmatch = [command.pattern];
-                    }
-                    if (cmmatch.endsWith(' ')) {
-                        var cmmatch = command.pattern.toString().match(/(\W*)([A-Za-züşiğ öç1234567890]*)/)[2].replace(' ', '')
-                    }
-                    if (cmmatch == match[1]) {
-                        var HANDLER = '';
-    
-                        if (/\[(\W*)\]/.test(Config.HANDLERS)) {
-                            HANDLER = Config.HANDLERS.match(/\[(\W*)\]/)[1][0];
-                        } else {
-                            HANDLER = '.';
-                        }
-                        if (command.desc == '' && !command.usage == '' && command.warn == '') {
-                            CMD_HELP += (match.length >= 3 ? (HANDLER + mmatch) : command.pattern) + '\n' + Lang.EXAMPLE + ': ' + command.usage + '\n\n';
-                        }
-                        if (!command.desc == '' && command.usage == '' && command.warn == '') {
-                            CMD_HELP += (match.length >= 3 ? (HANDLER + mmatch) : command.pattern) + '\n' +  command.desc + ' \n\n';
-                        }
-                        if (command.desc == '' && command.usage == '' && !command.warn == '') {
-                            CMD_HELP += (match.length >= 3 ? (HANDLER + mmatch) : command.pattern) + '\n' +  command.warn + '\n\n'
-                        }
-                        if (!command.desc == '' && !command.usage == '' && command.warn == '') {
-                            CMD_HELP += (match.length >= 3 ? (HANDLER + mmatch) : command.pattern) + '\n' +  command.desc + ' \n' + Lang.EXAMPLE + ': ' + command.usage + '\n\n';
-                        }
-                        if (!command.desc == '' && command.usage == '' && !command.warn == '') {
-                            CMD_HELP += (match.length >= 3 ? (HANDLER + mmatch) : command.pattern) + '\n' +  command.desc + ' \n' + Lang.WARN + ': ' + command.warn + '\n\n'
-                        }
-                        if (command.desc == '' && !command.usage == '' && !command.warn == '') {
-                            CMD_HELP += (match.length >= 3 ? (HANDLER + mmatch) : command.pattern) + '\n' +  command.usage + '\n' + Lang.WARN + ': ' + command.warn + '\n\n'
-                        }
-                        if  (command.desc == '' && command.usage == '' && command.warn == '') {
-                            CMD_HELP += (match.length >= 3 ? (HANDLER + mmatch) : command.pattern) + '\n\n'
-                        }
-                        if  (!command.desc == '' && !command.usage == '' && !command.warn == '') {
-                            CMD_HELP += (match.length >= 3 ? (HANDLER + mmatch) : command.pattern) + '\n' +  command.desc + ' \n' + Lang.EXAMPLE + ': ' + command.usage + '\n' + Lang.WARN + ': ' + command.warn + '\n\n'
-                        }
-                    }
-                }
-            );
-            if (CMD_HELP === '') CMD_HELP += Lang.NOT_FOUND;
-            message.sendMessage(CMD_HELP, {
-      quoted: {
-        key: {
-          participant: "0@s.whatsapp.net",
-          remoteJid: "status@broadcast",
-        },
-        message: {
-          orderMessage: {
-            itemCount: 123,
-            itemCoun: 404,
-            surface: 404,
-            message: `© ᴀʙᴜ-ᴍᴅ`,
-            orderTitle: "B",
-            thumbnail: img,
-            sellerJid: "0@s.whatsapp.net",
-          },
-        },
-      },
-    });
-  }
-);
-
-    
-    
-    
-    
