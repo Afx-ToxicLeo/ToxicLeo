435c250a4e81ad447bb0d243125f8c99c7c97d38
('Jsl') <100409086+Afx-Abu@users.noreply.github.com>
2022-09-22 17:24:10 +0530

    Fix 🤩🤌🏻

--- plugins/fancy.js
+++ plugins/fancy.js
@@ -5,17 +5,15 @@ const {
      Fancy
  } = require('../lib/misc')
  
- Jsl({
-     pattern: 'fancy ?(.*)',
-     fromMe: true,
-     use: 'misc',
-     desc: 'Creates fancy text fonts'
- }, (async (message, match,m) => {
-     if (!match[1] || !message.reply_message.message) return await message.reply("Reply to any message with .fancy number\n" + Fancy("example", "32"))
-     try {
-         var result = Fancy(message.reply_message.message, match[1])
-     } catch (e) {
-         return await message.reply(e.message)
-     }
+ Jsl({pattern: "fancy ?(.*)", 
+     fromMe: true, 
+     desc: "change text to fancy text fonts", 
+     type: "misvlc"}, 
+     async (message, match) => {
+ try {
+     var result = Fancy(message.reply_message.text, 31)
+ } catch (e) {
+     return await message.reply(e.message)
+ }
      await message.reply(result)
- }))
+});
