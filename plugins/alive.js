6ed20410fac7c51591e0cf8fa350b6f134560ecc
('Jsl') <100409086+Afx-Abu@users.noreply.github.com>
2022-09-22 10:16:59 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -11,19 +11,21 @@ Jsl(
     type: "misc",
   },
   async (message, match) => {
-   const buttons = [
-      {buttonId: ',menu', buttonText: {displayText: '𝙼𝙴𝙽𝚄'}},
-      {buttonId: ',ping', buttonText: {displayText: '𝙿𝙸𝙽𝙶'}}
-    ]
-    
-    const buttonMessage = {
+  const templateButtons = [
+  {index: 1, urlButton: {displayText: '⭐ GitHub!', url: 'https://github.com/Afx-Abu/Abu-MD'}},
+  {index: 2, callButton: {displayText: 'Contact!', phoneNumber: 'Wa.me/917025994178'}},
+  {index: 3, quickReplyButton: {displayText: '𝙼𝙴𝙽𝚄!', id: ',menu'}},
+  {index: 4, quickReplyButton: {displayText: '𝙿𝙸𝙽𝙶!', id: ',ping'}},
+]
+      
+    const templateMessage = {
         text: '      \n╔══╗╔╗─╔══╗╔╗─╔╗╔═╗        \n║╔╗║║║─╚║║╝║╚╦╝║║╦╝        \n║╠╣║║╚╗╔║║╗╚╗║╔╝║╩╗        \n╚╝╚╝╚═╝╚══╝─╚═╝─╚═╝ \n\n *╔✘ 𝐈𝐀𝐌 𝐒𝐓𝐈𝐋𝐋 𝐀𝐋𝐈𝐕𝐄 𝐁𝐑𝐎*»\n*║✘Bᴏᴛ ɴᴀᴍᴇ :*ᴀʙᴜ ᴍᴅ\n*║✘ Oᴡɴᴇʀ : ᴊsʟ*\n*║✘Wᴏʀᴋ ᴛʏᴘᴇ : *ᴘᴜʙʟɪᴄ\n*║✘ Pʀᴇғɪx : ' + Config.HANDLERS + '*    \n *╚✘*\n           *𝐀𝐁𝐔 𝐌𝐃 𝐖𝐀 𝐁𝐎𝐓*         \n\n*╭◪ ᴄᴏᴅᴅᴇᴅ ʙʏ ᴊsʟ*\n*╰─────────────────❋ཻུ۪۪⸙*\n',          
         footer: `${ALIVE_INFO.split(";")[0]}`,
-        buttons: buttons,
-        headerType: 1
-    }
+        templateButtons: templateButtons,
+        headerType: 5
+      }
     
-    await message.client.sendMessage(message.jid, buttonMessage)
+    await message.client.sendMessage(message.jid, templateButtons)
   }
 );
 
