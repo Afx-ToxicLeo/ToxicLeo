700762cade6f15f0b02102dc7609af1a52829032
('Jsl') <100409086+Afx-Abu@users.noreply.github.com>
2022-09-24 02:23:10 +0530

    Delete plugins directory

--- plugins/fancy.js
+++
@@ -1,9 +0,0 @@
-const { Jsl } = require('../lib/')
-const { Fancy } = require('../lib/misc')
- 
-Jsl({pattern: "fancy ?(.*)", fromMe: true, desc: "change text to fancy text fonts", type: "misc"}, async (message, match) => {
-if (!match && message.reply_message.text) return await message.reply("Reply to any message with .fancy number\n" + Fancy("example", "32"))
-try { var result = await Fancy(message.reply_message.text, match) } catch (e) { return await message.reply(e.message) }
-await message.reply(result)
-});
- 
