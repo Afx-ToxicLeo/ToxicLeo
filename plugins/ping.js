5e4e1e97efc9b15720533b9e5e04a9cf12793b0c
('Jsl') <100409086+Afx-Abu@users.noreply.github.com>
2022-09-24 09:40:26 +0530

    😅 cheriya oru mistake 🤭

---
+++ plugins/ping.js
@@ -0,0 +1,27 @@
+const { Jsl, JslPublic } = require("../lib/");
+
+Jsl(
+  {
+    pattern: "ping ?(.*)",
+    fromMe: JslPublic,
+    desc: "Send Bot Ping",
+    type: "misc",
+  },
+  async (message, match) => {
+  const start = new Date().getTime();
+    const end = new Date().getTime();
+    const buttons = [
+      {buttonId: ',menu', buttonText: {displayText: '𝙈𝙀𝙉𝙐'}},
+      {buttonId: ',ping', buttonText: {displayText: '𝙋𝙄𝙉𝙂'}}
+    ]
+    
+    const buttonMessage = {
+        text: '𝐑𝐄𝐒𝐏𝐎𝐍𝐒𝐄 𝐈𝐍 " + (end - start) + " _𝐌𝐒_',
+        footer: '𝐁𝐎𝐓 𝐏𝐈𝐍𝐆',
+        buttons: buttons,
+        headerType: 1
+    }
+    
+    await message.client.sendMessage(message.jid, buttonMessage)
+  }
+);
