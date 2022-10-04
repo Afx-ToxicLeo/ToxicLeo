a1704f1171e97436deb735d69e7d7e33ddbbd76a
('Jsl') <100409086+Afx-Abu@users.noreply.github.com>
2022-10-04 18:22:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,51 @@
-const toBool = (x) => x == 'true'
-const { Sequelize } = require('sequelize')
-const { existsSync } = require('fs')
-if (existsSync('config.env')) require('dotenv').config({ path: './config.env' })
-const DATABASE_URL = process.env.DATABASE_URL === undefined ? './database.db' : process.env.DATABASE_URL
+const { Sequelize } = require("sequelize");
+const fs = require("fs");
+if (fs.existsSync("config.env"))
+  require("dotenv").config({ path: "./config.env" });
+
+const toBool = (x) => x == "true";
+
+DATABASE_URL = process.env.DATABASE_URL || "./database.db";
+let HANDLER = "false";
 module.exports = {
-    VERSION: 'v1.0.0',
-    SESSION_ID: (process.env.SESSION_ID || '').trim(),
-    MODE: process.env.MODE || 'private',
-    DATABASE: DATABASE_URL === './database.db' ? new Sequelize({ dialect: 'sqlite', storage: DATABASE_URL, logging: false }) : new Sequelize(DATABASE_URL, {dialect: 'postgres', ssl: true, protocol: 'postgres', dialectOptions: { native: true, ssl: { require: true, rejectUnauthorized: false },}, logging: false }),
-    HANDLERS: process.env.PREFIX || '^[.,!]',
-    SUDO: process.env.SUDO || '917025994178',
-    HEROKU_APP_NAME: process.env.HEROKU_APP_NAME,
-    HEROKU_API_KEY: process.env.HEROKU_API_KEY,
-    BOT_INFO: process.env.BOT_INFO || 'Abu;Jsl;917025994178;https://i.imgur.com/abu.jpeg',
-    STICKER_PACKNAME: process.env.STICKER_PACKNAME || 'Abu;Jsl',
-    READ_MSG: process.env.READ_MSG || 'true', 
-    LOG_MSG: toBool(process.env.LOG_MSG) || false,
-    LANG: (process.env.LANGUAGE || 'english').toLowerCase(),
+  ANTILINK: toBool(process.env.ANTI_LINK) || false,
+  ANTILINK_ACTION: process.env.ANTI_LINK || "kick",
+  LANG: process.env.LANG || "EN",
+  HANDLERS:
+    process.env.HANDLER === "false" || process.env.HANDLER === "null"
+      ? "^"
+      : "^[,]",
+  SESSION_ID: (process.env.SESSION_ID || '').trim(),
+  RMBG_KEY: process.env.RMBG_KEY || false,
+  BRANCH: "master",
+  PACKNAME: process.env.PACKNAME || "Abu",
+  WELCOME_MSG:
+    process.env.WELCOME_MSG ||
+    "{pp}Hi @user Welcome to @gname\nYou're our @count/513 Members ",
+  GOODBYE_MSG: process.env.GOODBYE_MSG || "Hi @user It was Nice Seeing you",
+  AUTHOR: process.env.AUTHOR || "Jsl",
+  DATABASE_URL: DATABASE_URL,
+  DATABASE:
+    DATABASE_URL === "./database.db"
+      ? new Sequelize({
+          dialect: "sqlite",
+          storage: DATABASE_URL,
+          logging: false,
+        })
+      : new Sequelize(DATABASE_URL, {
+          dialect: "postgres",
+          ssl: true,
+          protocol: "postgres",
+          dialectOptions: {
+            native: true,
+            ssl: { require: true, rejectUnauthorized: false },
+          },
+          logging: false,
+        }),
+  SUDO: process.env.SUDO || "917025994178",
+  HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || " ",
+  HEROKU_API_KEY: process.env.HEROKU_API_KEY || " ",
+  OWNER_NAME: process.env.OWNER_NAME || "Abu",
+  BOT_NAME: process.env.BOT_NAME || "Abu-MD",
+  WORK_TYPE: process.env.WORK_TYPE || "public",
 };
