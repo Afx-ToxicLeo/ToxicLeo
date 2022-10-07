6b249cc04ce5b58a2e9652f636d4a3afd787c0cc
('Jsl') <100409086+Afx-Abu@users.noreply.github.com>
2022-10-07 10:15:56 +0530

    Create alive.js

---
+++ plugins/alive.js
@@ -0,0 +1,18 @@
+const {
+  Module,
+  isPublic
+} = require('../lib/')
+const {
+  ALIVE
+} = require('../config');
+const config = require('../config');
+const {
+  parseAlive
+} = require('../lib/misc/misc');
+Module({
+  pattern: 'alive',
+  fromMe: isPublic,
+  desc: 'Is bot alive?'
+}, (async (message, match) => {
+  await parseAlive(message, ALIVE)
+}))
