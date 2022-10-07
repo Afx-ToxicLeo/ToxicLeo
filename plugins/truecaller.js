05e392863c518987c4c8925b327783861dda7250
('Jsl') <100409086+Afx-Abu@users.noreply.github.com>
2022-10-07 11:01:08 +0530

    Update truecaller.js

--- plugins/truecaller.js
+++ plugins/truecaller.js
@@ -1,20 +1,13 @@
 const {find} = require('abu-bot')
-const {bot, isPublic} = require('../lib/');
-bot({pattern: 'true ?(.*)', desc: 'Searches for number in truecaller!',type: 'search',fromMe: isPublic}, async (msg, query) => {
-if (!query[1] && !msg.reply_message) return await msg.reply("_Give me any number or reply to any user!_");
-if (query[1].includes('/')) return await message.sendMessage('Wrong format! \n\n .true +91 7025994178')
-var go;
-if (msg.reply_message) go = msg.reply_message.jid.split('@')[0]
-else if (!query[1].includes('@')) go = query[1]
-else if (msg.mention) {
-var mm = '';
-msg.mention.map(async (user) => {
-mm += user.split('@')[0];
+const {bot, isPublic } = require('../lib/');
+bot({pattern: 'true ?(.*)', desc: 'Searches for number in truecaller!',type: 'search',fromMe: isPublic}, async (message, query) => {
+let user =  (message.mention[0] || message.reply_message?.jid || query[1]).split("@")[0]
+if (!user) return await message.reply("_Need number/reply/mention_");
+const trueCaller = async (num) => {try { var res = await find(num,'',message.client.user.id) } catch { var res = false }; return res;}
+for (let i = 0; i < 6; i++){
+    var _result = await trueCaller(user.replace( /^\D| +/g, ''))
+    if (i === 5 || _result === false) {
+        return await message.sendMessage("_Error, try again!_")
+    } else return await message.sendMessage(_result)
+}
 });
-go = mm
-} 
-var initt = go.split(" ").join("")
-var number = initt.replace('+','')
-var res = await find(number,'',msg.client.user.id)
-if (res == 'error') return await message.sendMessage("_Truecaller limit over! (50/50) Contact owner_")
-await message.sendMessage(res);});
