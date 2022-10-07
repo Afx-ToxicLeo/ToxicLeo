8b53e8c8afbeb39cbe9f0631b576cff6acada2b4
('Jsl') <100409086+Afx-Abu@users.noreply.github.com>
2022-10-07 11:33:41 +0530

    Update url.js

--- plugins/url.js
+++ plugins/url.js
@@ -18,7 +18,7 @@ if (m.reply_message.sticker){
 else if (m.reply_message.audio){
     if (m.reply_message.duration > 90) return await m.client.sendMessage(m.jid,{text:'_Audio too large. Try below 90 seconds!_'},{quoted: m.quoted});
     var q = await m.reply_message.download();
-    await m.sendReply("_Converting to video & uploading..._")
+    await m.reply("_Converting to video & uploading..._")
     ffmpeg(q).outputOptions(["-y", "-filter_complex", "[0:a]showvolume=f=1:b=4:w=720:h=68,format=yuv420p[vid]", "-map", "[vid]", "-map 0:a"]).save('output.mp4').on('end', async () => {
     try { var res = await upload('output.mp4'); } catch {return await m.client.sendMessage(m.jid,{text:"_Failed to upload file!_"},{quoted: m.quoted});}
     return await m.client.sendMessage(m.jid,{text:"_"+res.link+"_"},{quoted: m.quoted});
@@ -27,5 +27,5 @@ else if (m.reply_message.audio){
 else if (m.reply_message.image || m.reply_message.video){
 try { await m.client.sendMessage(m.jid,{text:"_"+(await upload(await m.reply_message.download())).link+"_"},{quoted: m.quoted}) } catch {return await m.client.sendMessage(m.jid,{text:"_Failed to upload file!_"},{quoted: m.quoted});}
 }
-else return await m.sendReply("_Reply to image|video|audio|sticker_");
+else return await m.reply("_Reply to image|video|audio|sticker_");
 });
