59b80d1cccc0c72181b19ae91588f11e44258aa0
('Jsl') <100409086+Afx-Abu@users.noreply.github.com>
2022-10-08 23:52:03 +0530

    Create _user.js

---
+++ plugins/_user.js
@@ -0,0 +1,109 @@
+const { Module } = require("../lib");
+
+
+Module(
+  {
+    pattern: "setpp ",
+    fromMe: true,
+    desc: "Set profile picture",
+    type: "user",
+  },
+  async (message, match, m) => {
+    if (!message.reply_message.image)
+      return await message.reply("_Reply to a photo_");
+    let buff = await m.quoted.download();
+    await message.setPP(message.user, buff);
+    return await message.reply("_Profile Picture Updated_");
+  }
+);
+
+
+Module(
+  {
+    pattern: "setname",
+    fromMe: true,
+    desc: "Set User name",
+    type: "user",
+  },
+  async (message, match) => {
+    if (!match) return await message.reply("_Enter name_");
+    await message.updateName(match);
+    return await message.reply(`_Username Updated : ${match}_`);
+  }
+);
+
+
+Module(
+  {
+    pattern: "block",
+    fromMe: true,
+    desc: "Block a person",
+    type: "user",
+  },
+  async (message, match) => {
+    if (message.isGroup) {
+      let jid = message.mention[0] || message.reply_message.jid;
+      if (!jid) return await message.reply("_Reply to a person or mention_");
+      await message.block(jid);
+      return await message.sendMessageMessage(`_@${jid.split("@")[0]} Blocked_`, {
+        mentions: [jid],
+      });
+    } else {
+      await message.block(message.jid);
+      return await message.reply("_User blocked_");
+    }
+  }
+);
+
+
+Module(
+  {
+    pattern: "unblock",
+    fromMe: true,
+    desc: "Unblock a person",
+    type: "user",
+  },
+  async (message, match) => {
+    if (message.isGroup) {
+      let jid = message.mention[0] || message.reply_message.jid;
+      if (!jid) return await message.reply("_Reply to a person or mention_");
+      await message.block(jid);
+      return await message.sendMessage(`_@${jid.split("@")[0]} unblocked_`, {
+        mentions: [jid],
+      });
+    } else {
+      await message.unblock(message.jid);
+      return await message.reply("_User unblocked_");
+    }
+  }
+);
+
+
+Module(
+  {
+    pattern: "jid",
+    fromMe: true,
+    desc: "Give jid of chat/user",
+    type: "user",
+  },
+  async (message, match) => {
+    return await message.sendMessage(
+      message.mention[0] || message.reply_message.jid || message.jid
+    );
+  }
+);
+
+
+Module(
+  {
+    pattern: "dlt",
+    fromMe: true,
+    desc: "deletes a message",
+    type: "user",
+  },
+  async (message, match,m,client) => {
+    if (message.isGroup) {
+      client.sendMessage(message.jid, { delete: message.reply_message.key })
+    }
+  }
+);
