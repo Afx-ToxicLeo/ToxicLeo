14b0413c8078bcfcbea45db3b799b0ffd1294716
('Jsl') <100409086+Afx-Abu@users.noreply.github.com>
2022-10-09 12:09:25 +0530

    fix

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ if (fs.existsSync("config.env"))
 
 const toBool = (x) => x == "true";
 
-DATABASE_URL = process.env.DATABASE_URL || "./database.db";
+DATABASE: DATABASE_URL === './database.db' ? new Sequelize({ dialect: 'sqlite', storage: DATABASE_URL, logging: false }) : new Sequelize(DATABASE_URL, {dialect: 'postgres', ssl: true, protocol: 'postgres', dialectOptions: { native: true, ssl: { require: true, rejectUnauthorized: false },}, logging: false }),
 let HANDLER = "false";
 module.exports = {
   VERSION: 'v1.0.0',
