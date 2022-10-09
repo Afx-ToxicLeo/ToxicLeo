c9e7ad24ac73ddd591ec00e96d9300fd3e226d31
('Jsl') <100409086+Afx-Abu@users.noreply.github.com>
2022-10-09 20:02:07 +0530

    Update testing.js

--- plugins/testing.js
+++ plugins/testing.js
@@ -13,3 +13,19 @@ Module({
 await message.client.sendMessage(message.chat, { text: `*MENTION :* @${message.sender.split("@")[0]}`, mentions: [message.sender] })
 
 });
+
+Module({
+	pattern: 'reaction ?(.*)',
+	fromMe: isPublic,
+	desc: 'Sends reaction',
+	type: 'misc',
+}, async (message, match) => {
+	const reactionMessage = {
+    react: {
+        text: "💖", // Your emoji here
+        key: message.data.key
+    }
+}
+
+await message.client.sendMessage(message.chat, reactionMessage)
+});
