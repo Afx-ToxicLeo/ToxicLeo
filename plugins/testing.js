7c4b52575601e2ffbaa852c053c7e11695a052b0
('Jsl') <100409086+Afx-Abu@users.noreply.github.com>
2022-10-09 20:14:14 +0530

    Update testing.js

--- plugins/testing.js
+++ plugins/testing.js
@@ -4,15 +4,59 @@ const {
 } = require('../lib/')
 
 Module({
+	pattern: 'text ?(.*)',
+	fromMe: isPublic,
+	desc: 'Sends text',
+	type: 'misc',
+}, async (message, match) => {
+	await message.send("*TEXT HERE*")
+});
+
+// ------------------------------------------------------------------------------------------------
+//              IMAGE SENDER SAMPLE 👇
+// ------------------------------------------------------------------------------------------------
+
+Module({
+	pattern: 'image ?(.*)',
+	fromMe: isPublic,
+	desc: 'Sends image',
+	type: 'misc',
+}, async (message, match) => {
+	await message.client.sendMessage(message.jid, {image: {url: 'https://i.imgur.com/biqxAoi.jpeg'}, caption: "YOUR CAPTION HERE"})
+});
+
+// ------------------------------------------------------------------------------------------------
+//              VIDEO SENDER SAMPLE 👇
+// ------------------------------------------------------------------------------------------------
+
+Module({
+	pattern: 'video ?(.*)',
+	fromMe: isPublic,
+	desc: 'Sends video',
+	type: 'misc',
+}, async (message, match) => {
+	await message.client.sendMessage(message.jid, {video: {url: 'https://i.imgur.com/NCifJWe.mp4'}, caption: "YOUR CAPTION HERE"})
+});
+
+
+// ------------------------------------------------------------------------------------------------
+//          MENTION TEXT SENDER SAMPLE 👇
+// ------------------------------------------------------------------------------------------------
+
+Module({
 	pattern: 'mentiontext ?(.*)',
 	fromMe: isPublic,
 	desc: 'Sends mention text',
 	type: 'misc',
 }, async (message, match) => {
 
-await message.client.sendMessage(message.chat, { text: `*MENTION :* @${message.sender.split("@")[0]}`, mentions: [message.sender] })
+await message.client.sendMessage(message.jid, { text: `*MENTION :* @${message.sender.split("@")[0]}`, mentions: [message.sender] })
 
 });
+
+// ------------------------------------------------------------------------------------------------
+//          REACTION SENDER SAMPLE 👇
+// ------------------------------------------------------------------------------------------------
 
 Module({
 	pattern: 'reaction ?(.*)',
@@ -27,8 +71,190 @@ Module({
     }
 }
 
-await message.client.sendMessage(message.chat, reactionMessage)
+await message.client.sendMessage(message.jid, reactionMessage)
 });
+
+// ------------------------------------------------------------------------------------------------
+//          LIST BUTTON SENDER SAMPLE 👇
+// ------------------------------------------------------------------------------------------------
+
+Module({
+	pattern: 'listbutton ?(.*)',
+	fromMe: isPublic,
+	desc: 'Sends list button',
+	type: 'misc',
+}, async (message, match) => {
+
+const sections = [
+    {
+	title: "Section 1",
+	rows: [
+	    {title: "Option 1", rowId: "option1"},
+	    {title: "Option 2", rowId: "option2", description: "This is a description"}
+	]
+    },
+   {
+	title: "Section 2",
+	rows: [
+	    {title: "Option 3", rowId: "option3"},
+	    {title: "Option 4", rowId: "option4", description: "This is a description V2"}
+	]
+    },
+]
+
+const listMessage = {
+  text: "This is a list",
+  footer: "nice footer, link: https://google.com",
+  title: "Amazing boldfaced list title",
+  buttonText: "Required, text on the button to view the list",
+  sections
+}
+
+await message.client.sendMessage(message.jid, listMessage)
+})
+
+
+// ------------------------------------------------------------------------------------------------
+//          BUTTON TEXT SENDER SAMPLE 👇
+// ------------------------------------------------------------------------------------------------
+
+Module({
+	pattern: 'buttontext ?(.*)',
+	fromMe: isPublic,
+	desc: 'Sends button text',
+	type: 'misc',
+}, async (message, match) => {
+
+const buttons = [
+  {buttonId: 'id1', buttonText: {displayText: 'Button 1'}, type: 1},
+  {buttonId: 'id2', buttonText: {displayText: 'Button 2'}, type: 1},
+  {buttonId: 'id3', buttonText: {displayText: 'Button 3'}, type: 1}
+]
+
+const buttonMessage = {
+    text: "Hi it's button message",
+    footer: 'Hello World',
+    buttons: buttons,
+    headerType: 1
+}
+
+await message.client.sendMessage(message.jid, buttonMessage)
+
+})
+
+
+// ------------------------------------------------------------------------------------------------
+//          BUTTON IMAGE SENDER SAMPLE 👇
+// ------------------------------------------------------------------------------------------------
+
+Module({
+	pattern: 'buttonimage ?(.*)',
+	fromMe: isPublic,
+	desc: 'Sends button image',
+	type: 'misc',
+}, async (message, match) => {
+
+const buttons = [
+  {buttonId: 'id1', buttonText: {displayText: 'Button 1'}, type: 1},
+  {buttonId: 'id2', buttonText: {displayText: 'Button 2'}, type: 1},
+  {buttonId: 'id3', buttonText: {displayText: 'Button 3'}, type: 1}
+]
+
+const buttonMessage = {
+    image: {url: 'https://i.imgur.com/biqxAoi.jpeg'},
+    caption: "Hi it's button message",
+    footer: 'Hello World',
+    buttons: buttons,
+    headerType: 1
+}
+
+await message.client.sendMessage(message.jid, buttonMessage)
+
+})
+
+// ------------------------------------------------------------------------------------------------
+//          BUTTON VIDEO SENDER SAMPLE 👇
+// ------------------------------------------------------------------------------------------------
+
+Module({
+	pattern: 'buttonvideo ?(.*)',
+	fromMe: isPublic,
+	desc: 'Sends button video',
+	type: 'misc',
+}, async (message, match) => {
+
+const buttons = [
+  {buttonId: 'id1', buttonText: {displayText: 'Button 1'}, type: 1},
+  {buttonId: 'id2', buttonText: {displayText: 'Button 2'}, type: 1},
+  {buttonId: 'id3', buttonText: {displayText: 'Button 3'}, type: 1}
+]
+
+const buttonMessage = {
+    video: {url: 'https://i.imgur.com/NCifJWe.mp4'},
+    caption: "Hi it's button message",
+    footer: 'Hello World',
+    buttons: buttons,
+    headerType: 1
+}
+
+await message.client.sendMessage(message.jid, buttonMessage)
+
+})
+
+// ------------------------------------------------------------------------------------------------
+//          TEMPLATE BUTTON TEXT SENDER SAMPLE 👇
+// ------------------------------------------------------------------------------------------------
+
+Module({
+	pattern: 'templatebuttontext ?(.*)',
+	fromMe: isPublic,
+	desc: 'Sends template button text',
+	type: 'misc',
+}, async (message, match) => {
+
+const templateButtons = [
+    {index: 1, urlButton: {displayText: '⭐ Star Abu on GitHub!', url: 'https://github.com/Abu/Abu'}},
+    {index: 2, callButton: {displayText: 'Call me!', phoneNumber: '+65 7777 5555'}},
+    {index: 3, quickReplyButton: {displayText: 'This is a reply, just like normal buttons!', id: 'id-like-buttons-message'}},
+]
+
+const templateMessage = {
+    text: "Hi it's a template message",
+    footer: 'Hello World',
+    templateButtons: templateButtons
+}
+
+await message.client.sendMessage(message.jid, templateMessage)
+
+})
+
+// ------------------------------------------------------------------------------------------------
+//          TEMPLATE BUTTON IMAGE SENDER SAMPLE 👇
+// ------------------------------------------------------------------------------------------------
+
+Module({
+	pattern: 'templatebuttonimage ?(.*)',
+	fromMe: isPublic,
+	desc: 'Sends template button image',
+	type: 'misc',
+}, async (message, match) => {
+
+const templateButtons = [
+    {index: 1, urlButton: {displayText: 'GitHub!', url: 'https://github.com/Afx-Abu/Abu-MD'}},
+    {index: 2, callButton: {displayText: 'Call me!', phoneNumber: '+91 7025994178'}},
+    {index: 3, quickReplyButton: {displayText: 'Menu!', id: '.menu'}},
+]
+
+const templateMessage = {
+    image: {url: 'https://i.imgur.com/biqxAoi.jpeg'},
+    caption: "Hi it's a template message",
+    footer: 'Hello World',
+    templateButtons: templateButtons
+}
+
+await message.client.sendMessage(message.jid, templateMessage)
+
+})
 
 Module({
 	pattern: 'temp ?(.*)',
@@ -50,7 +276,7 @@ const templateMessage = {
     templateButtons: templateButtons
 }
 
-await message.client.sendMessage(message.chat, templateMessage)
+await message.client.sendMessage(message.jid, templateMessage)
 
 })
 
@@ -69,7 +295,7 @@ const vcard = 'BEGIN:VCARD\n'
             + 'TEL;type=CELL;type=VOICE;waid=917025994178:+91 7025994178\n'
             + 'END:VCARD'
 await message.client.sendMessage(
-    message.chat,
+    message.jid,
     { 
         contacts: { 
             displayName: 'Jeff', 
@@ -88,7 +314,7 @@ Module({
 }, async (message, match) => {
 
 await message.client.sendMessage(
-    message.chat, 
+    message.jid, 
     { location: { degreesLatitude: 24.121231, degreesLongitude: 55.1121221 } }
 )
 
